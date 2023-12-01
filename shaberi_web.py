f5924e5166c9362dec915fe06b99e1946f668039
Frandy <frandyfancy@gmail.com>
2023-12-01 18:22:40 +0800

    add delete directory function

--- shaberi_web.py
+++ shaberi_web.py
@@ -1,15 +1,28 @@
 from selenium import webdriver
 import time
+import shutil
+import os
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-import os
 from selenium.webdriver.chrome.options import Options
 
-browser = webdriver.Chrome()
+
+download_directory = os.path.expanduser('~/Downloads/unit_test_folder')
+# [建立]測試資料
+os.makedirs(download_directory)
+chrome_options = Options()
+chrome_options.add_experimental_option('prefs', {
+    'download.default_directory': download_directory,
+    'download.prompt_for_download': False,
+    'download.directory_upgrade': True,
+    'safebrowsing.enabled': False,
+    'safebrowsing.disable_download_protection': True,
+})
+browser = webdriver.Chrome(options=chrome_options)
 browser.maximize_window()
 wait = WebDriverWait(browser, 10)
 actions = ActionChains(browser)
@@ -17,7 +30,7 @@ current_url = browser.current_url
 browser.get("https://www.shaberi.com/tw")
 
 
-#language test-------------
+# # language test-------------
 # language_dropdown = browser.find_element(By.CLASS_NAME, 'flex.cursor-pointer')
 # language_dropdown.click()
 # language_chinese_cn = browser.find_element(By.XPATH, '//*[@id="__layout"]/div/header/div[1]/div/div/div/ul/li[1]/a')
@@ -41,7 +54,7 @@ browser.get("https://www.shaberi.com/tw")
 # language_chinese = browser.find_element(By.XPATH, '//*[@id="__layout"]/div/header/div[1]/div/div/div/ul/li[4]/a')
 # language_chinese.click()
 # wait.until(EC.url_contains("/tw"))
-# time.sleep(5)
+# time.sleep(1)
 
 # #download test -----------
 # apple_store = browser.find_element(By.XPATH, '//*[@id="__layout"]/div/main/section[1]/div/div[1]/div/div[1]/div/a[1]')
@@ -67,70 +80,99 @@ browser.get("https://www.shaberi.com/tw")
 # wait.until(EC.url_to_be("https://apps.apple.com/us/app/%E8%99%BE%E8%B4%9D/id6451127792"))
 # browser.close()
 
-browser.switch_to.window(browser.window_handles[0])
-download_directory = os.path.expanduser('~/Downloads')
-chrome_options = Options()
-chrome_options.add_experimental_option('prefs', {
-    'download.default_directory': download_directory,
-    'download.prompt_for_download': False,
-    'download.directory_upgrade': True,
-    'safebrowsing.enabled': False,
-    'safebrowsing.disable_download_protection': True,
-})
-# print("testing android apk download url")
-# apk_bt = browser.find_element(By.XPATH, '//*[@id="__layout"]/div/main/section[1]/div/div[1]/div/div[2]/div/a[2]')
-# apk_bt.click()
-# apk_url = apk_bt.get_attribute("href").split("/")[-1]
-# try:
-#     # 等待下载完成，最多等待10秒
-#     wait_time = 0
-#     is_download_ok = False
-#     while wait_time < 10 and not is_download_ok:
-#         downloaded_files = os.listdir(download_directory)
-#         for files in downloaded_files:
-#             if apk_url in files:
-#                 print("[PASS]")
-#                 print("apk_url:",apk_url)
-#                 is_download_ok = True
-#                 break
-#         if not is_download_ok:
-#             wait_time += 1
-            # time.sleep(1)
-#     if not is_download_ok:
-#         print("[Fail] ")
-# finally:
-#     pass
+# browser.switch_to.window(browser.window_handles[0])
 
-print("testing macos download url")
-macos_bt = browser.find_element(By.XPATH, '//*[@id="__layout"]/div/main/section[1]/div/div[1]/div/div[3]/div/a[1]')
-macos_bt.click()
+
+
+
+
+
+
+print("testing android apk download url")
 time.sleep(1)
-macos_url = macos_bt.get_attribute("href").split("/")[-1]
-print("macos_url:",macos_url)
+apk_bt = browser.find_element(By.XPATH, '//*[@id="__layout"]/div/main/section[1]/div/div[1]/div/div[2]/div/a[2]')
+apk_bt.click()
+apk_url = apk_bt.get_attribute("href").split("/")[-1]
+apk_path = os.path.join(download_directory,apk_url)
 try:
     # 等待下载完成，最多等待10秒
     wait_time = 0
-    macos_is_download_ok = False
-    while wait_time < 15 and not macos_is_download_ok:
+    is_download_ok = False
+    while wait_time < 30 and not is_download_ok:
+        if wait_time % 5 == 0:
+            print(f"等待時間 {wait_time}")
         downloaded_files = os.listdir(download_directory)
         for files in downloaded_files:
-            if macos_url in files:
+            if "apk" in files:
+                print(f"找到的檔案有 {files}")
+            if apk_url == files.split("/")[-1]:
                 print("[PASS]")
-                print("macos_url:",macos_url)
-                
-                macos_is_download_ok = True
+                print("apk_url:",apk_url)
+                is_download_ok = True                                
                 break
-        if not macos_is_download_ok:
+        if not is_download_ok:
             wait_time += 1
             time.sleep(1)
-    if not macos_is_download_ok:
-        print("[Fail]")
+    if not is_download_ok:
+        print("[Fail] ")
+       
 finally:
     pass
 
 
+# [刪除]測試資料
+shutil.rmtree(download_directory)
 
+# print("testing macos download url")
+# macos_bt = browser.find_element(By.XPATH, '//*[@id="__layout"]/div/main/section[1]/div/div[1]/div/div[3]/div/a[1]')
+# macos_bt.click()
+# time.sleep(1)
+# # macos_url = macos_bt.get_attribute("href").split("/")[-1]
+# macos_url = '.crdownload'
+# try:
+#     # 等待下载完成，最多等待10秒
+#     wait_time = 0
+#     macos_is_download_ok = False
+#     while wait_time < 15 and not macos_is_download_ok:
+#         downloaded_files = os.listdir(download_directory)
+#         for file in downloaded_files:
+#             if file.endswith(macos_url):
+#                 print("[PASS]")
+#                 print("macos_url:",macos_url)
+#                 macos_is_download_ok = True
+#                 break
+#         if not macos_is_download_ok:
+#             wait_time += 1
+#             time.sleep(1)
+#     if not macos_is_download_ok:
+#         print("[Fail]")
+# finally:
+#     pass
 
+# print("testing windows download url")
+# windows_bt = browser.find_element(By.XPATH, '//*[@id="__layout"]/div/main/section[1]/div/div[1]/div/div[3]/div/a[2]')
+# windows_bt.click()
+# windows_url = windows_bt.get_attribute("href").split("/")[-1]
+# print(windows_url)
+# try:
+#     # 等待下载完成，最多等待10秒
+#     wait_time = 0
+#     windows_is_download_ok = False
+#     while wait_time < 10 and not windows_is_download_ok:
+#         downloaded_files = os.listdir(download_directory)
+#         for files in downloaded_files:
+#             if windows_url in files:
+#                 print("[PASS]")
+#                 print("windows_url:",windows_url)
+#                 windows_is_download_ok = True
+#                 break
+#         if not windows_is_download_ok:
+#             wait_time += 1
+#             time.sleep(1)
+#     if not windows_is_download_ok:
+#         print("[Fail] ")
+# finally:
+#     pass
 
 
 
