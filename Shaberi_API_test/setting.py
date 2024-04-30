c4df7b06d852dff90ffbb19d11e3178e7979bb70
frandy <frandy@frandys-mac-mini.local>
2024-04-30 17:25:05 +0800

    add get os environment function

--- Shaberi_API_test/setting.py
+++ Shaberi_API_test/setting.py
@@ -5,6 +5,7 @@ import csv
 import uuid
 import time
 import requests
+import os
 
 
 
@@ -15,35 +16,17 @@ def generate_device_id():
     hex_string = str(device_id).replace('-', '')
     return hex_string[:32]
 
-
-def send_email(subject_prefix, body):
-    current_time = datetime.now()
-    formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
-    #subject_prefix 由 Login , Wallet 帶入prefix 
-    subject = f"{subject_prefix} Test failed  {formatted_time}"
-    recipients = ["frandyfancy@gmail.com"]
-    sender = "frandyfancy@gmail.com"
-    password = "mwwdsbtomalgcmbh"
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = ', '.join(recipients)
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-       smtp_server.login(sender, password)
-       smtp_server.sendmail(sender, recipients, msg.as_string())
-    print("Message sent!")
-
-
 #env
 class Enviroment:
     def __init__(self):
-        self.env = "uat"
+        self.env = os.environ.get('ENVIRONMENT', "uat")
         self.urls = {
             "prod": "https://api.imser5yw.com",
             "uat": "https://im-uat.imdevs.net",
             "stg": "https://im-stg.imdevs.net",
             "dev": "https://im-dev.imdevs.net",
         }
+
         self.wallet_urls = {
             "prod": "https://svzyyges3qk6b6s62v88qts.letsgomars.com",
             "uat": "https://im-uat-aaj7xg4ds.imdevs.net",
@@ -91,6 +74,24 @@ class ReadCSV:
                     self.client_secret = row[1]
                 elif row[0] == "wallet_password":
                     self.wallet_password = row[1]
+
+
+def send_email(subject_prefix, body):
+    current_time = datetime.now()
+    formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+    #subject_prefix 由 Login , Wallet 帶入prefix 
+    subject = f"{subject_prefix} Test failed  {formatted_time}"
+    recipients = ["frandyfancy@gmail.com"]
+    sender = "frandyfancy@gmail.com"
+    password = "mwwdsbtomalgcmbh"
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
 
 def send_shaberi_message(global_token, message):
     get_environment_url = Enviroment().get_base_url
